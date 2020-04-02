f244745745743f4795147db118645401fb705eeb
supernovalx <hoang@DESKTOP-1A4GE92>
2020-04-02 18:02:38 +0700

    first commit

---
+++ Arduino led strip audio visualizer/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino_led_strip_audio_visualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
